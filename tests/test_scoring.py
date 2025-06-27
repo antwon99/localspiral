de8dac8dd73f5b353aaa5e482faefc3e95e97539
antwon99 <anthonyhoward@live.ca>
2025-06-27 14:09:51 -0400

    Add packaging and rename src to localspiral

--- tests/test_scoring.py
+++ tests/test_scoring.py
@@ -3,7 +3,7 @@ import sys
 
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-from src.utils.scoring import calculate_drift
+from localspiral.utils.scoring import calculate_drift
 
 import pytest
 
