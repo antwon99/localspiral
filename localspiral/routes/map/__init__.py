1a7522ac614bc490075d2e30d64ca64b17b845e5
antwon99 <anthonyhoward@live.ca>
2025-06-27 22:43:12 -0400

    Update __init__.py
    Merge Error

--- localspiral/routes/map/__init__.py
+++ localspiral/routes/map/__init__.py
@@ -1,6 +1,8 @@
 import random
 from flask import Blueprint, jsonify, request, session
 
+from ...utils.spiral_state import analyze_map
+
 from ...utils.map import generate_map
 
 map_bp = Blueprint('map', __name__)
@@ -20,4 +22,11 @@ def get_map():
 
     session['map_seed'] = seed
     grid = generate_map(seed)
+    analysis = analyze_map(grid)
+    location = (5, 5)
+    if 0 <= location[0] < len(grid) and 0 <= location[1] < len(grid[0]):
+        grid[location[0]][location[1]] = '@'
+    session['map_analysis'] = analysis
+    session['player_loc'] = location
+    return jsonify({'seed': seed, 'grid': grid, 'analysis': analysis, 'location': location})
     return jsonify({'seed': seed, 'grid': grid})
