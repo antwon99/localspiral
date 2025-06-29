9e470d393753fc06693d07883593dc8ca03a02ad
antwon99 <anthonyhoward@live.ca>
2025-06-29 19:38:05 -0400

    test: remove duplicate monkeypatch setup

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -29,10 +29,6 @@ def test_chat_endpoint(client, monkeypatch):
         'localspiral.utils.game_loop.add_enemy',
         lambda state: None
     )
-    monkeypatch.setattr(
-        'localspiral.utils.game_loop.add_enemy',
-        lambda state: None
-    )
     response = client.get('/chat?prompt=hello')
     assert response.status_code == 200
     data = response.get_json()
@@ -298,4 +294,3 @@ def test_move_increments_turn_count(client, monkeypatch):
     client.get('/map?seed=1')
     client.get('/move?dir=north')
     assert flask_session['game_state']['turn_count'] == 1
-
